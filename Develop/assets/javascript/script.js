c5cdd8a6de2f62fb29c598e514fece67012ad4a9
NickNocc <haretakis4@gmail.com>
2021-11-26 15:55:48 -0800

    api not auth'd so now forecast cards generate with no info

--- Develop/assets/javascript/script.js
+++ Develop/assets/javascript/script.js
@@ -1,7 +1,4 @@
 var api_Key = "de3eea8f17c1c1fc772302f0de5232c3";
-var api_Id = "Challenge-6"
-
-
 
 var getWeather = function(cityName) {
 
@@ -16,6 +13,7 @@ var getWeather = function(cityName) {
           console.log(response);
           response.json().then(function(data) {
             console.log(data);
+            dailyForecast(data);
           });
         } else {
           alert("Error: " + response.statusText);
@@ -24,10 +22,29 @@ var getWeather = function(cityName) {
       .catch(function(error) {
         alert("Unable to connect to OpenWeather");
       });
+
 };
+
+var dailyForecast = function(data) {
+    // City name + Date
+    // temp, wind, humidity, uv index
+}
+
+var fiveDayForecast = function() {
+    // for the following 5 days I need
+    var fiveDayTemp = "frick";
+    var fiveDayHumidity = "frack";
+    var fiveDayWind = "fruck";
+    for (var i = 0; i < 5; i++) {
+    $("#cardHolder").append("<div class='bg-primary col-2 mx-2'><h3>Date</h3> </br> <p>Temp: " + fiveDayTemp + "</p> </br> <p>Wind:"  + fiveDayWind +" </p> </br><p>Humidity: " + fiveDayHumidity + "</p> </br>")
+};
+    // Temp, wind, and humidity
+}
 
 $(".btn").on("click",  function() {
     var cityName = $("#citySearch").val();
     getWeather(cityName)
 });
+
+fiveDayForecast();
 
