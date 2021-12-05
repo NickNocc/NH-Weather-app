d46831f9e866ff9497d779e92b99e94ab1aef016
NickNocc <haretakis4@gmail.com>
2021-12-05 14:08:50 -0800

    bruh

--- Develop/assets/javascript/script.js
+++ Develop/assets/javascript/script.js
@@ -1,24 +1,30 @@
 var api_Key = "de3eea8f17c1c1fc772302f0de5232c3";
+var cityNameHtml = $("#cityName");
+var cityIcon = $("#cityIcon");
+var oneDayTemp = $("#temp");
+var oneDayHumidity = $("#humidity");
+var oneDayWindSpeend = $("#windspeed");
+var oneDayUV = $("#UV-Index");
 
 var getWeather = function(cityName) {
 
     // format the github api url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q={" + cityName + "}&appid={" + api_Key + "}";
-  
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + api_Key + "&units=imperial";
+
     // make a get request to url
     fetch(apiUrl)
       .then(function(response) {
         // request was successful
         if (response.ok) {
-          console.log(response);
           response.json().then(function(data) {
-            console.log(data);
+            var latitude = Math.round(data.coord.lat);
+            var longitude = Math.round(data.coord.lon);
+            console.log("lat: " + latitude + " Long: " + longitude)
+            fiveDayForecast(data);
             dailyForecast(data);
-          });
-        } else {
+        })} else {
           alert("Error: " + response.statusText);
-        }
-      })
+        })
       .catch(function(error) {
         alert("Unable to connect to OpenWeather");
       });
@@ -26,17 +32,29 @@ var getWeather = function(cityName) {
 };
 
 var dailyForecast = function(data) {
+  var cityName = data.name;
+  var weatherIcon = data.weather[0].icon
+  var currentDate = new Date(data.dt * 1000);
+  var day = currentDate.getDate();
+  var month = currentDate.getMonth() + 1;
+  var year = currentDate.getFullYear();
+  var dayTemp = Math.round(data.main.temp);
+  var dayWind = Math.round(data.wind.speed);
+  var dayHumidity = Math.round(data.main.humidity);
+  oneDayTemp.html("Temperature: " + dayTemp + " </br></br> Humidity: " + dayHumidity + " </br></br> Wind Speed: " + dayWind)
+  cityIcon.attr("src", "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
+  cityIcon.attr("alt", data.weather[0].description);
+  cityNameHtml.html(cityName + " (" + month + "/" + day + "/" + year + ") ");
     // City name + Date
     // temp, wind, humidity, uv index
 }
 
-var fiveDayForecast = function() {
-    // for the following 5 days I need
-    var fiveDayTemp = "frick";
-    var fiveDayHumidity = "frack";
-    var fiveDayWind = "fruck";
+var fiveDayForecast = function(data) {
+  var fiveDayTemp = Math.round(data.main.temp);
+  var fiveDayWind = Math.round(data.wind.speed);
+  var fiveDayHumidity = Math.round(data.main.humidity);
     for (var i = 0; i < 5; i++) {
-    $("#cardHolder").append("<div class='bg-primary col-2 mx-2'><h3>Date</h3> </br> <p>Temp: " + fiveDayTemp + "</p> </br> <p>Wind:"  + fiveDayWind +" </p> </br><p>Humidity: " + fiveDayHumidity + "</p> </br>")
+    $("#cardHolder").append("<div class='bg-primary col-2 mx-2' id=fiveCards><h3>Date</h3> </br> <p>Temp: " + fiveDayTemp + "F</p> </br> <p>Wind:"  + fiveDayWind +"mph </p> </br><p>Humidity: " + fiveDayHumidity + "%</p> </br>")
 };
     // Temp, wind, and humidity
 }
@@ -45,6 +63,3 @@ $(".btn").on("click",  function() {
     var cityName = $("#citySearch").val();
     getWeather(cityName)
 });
-
-fiveDayForecast();
-
